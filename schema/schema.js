873e1d0979e2f4d9fa3fb5fc5e82744cf8c148e1
Punkbooster <ugliar.arsen@gmail.com>
2020-02-06 15:54:27 +0100

    Add GraphQL sample working query

--- schema/schema.js
+++ schema/schema.js
@@ -1,17 +1,40 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
-const = {
+const users = [
+  { id: '23', firstName: 'Bill', age: 20},
+  { id: '43', firstName: 'Jake', age: 33}
+]
+
+const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql
 
-
-const UserType = new GraphQLObjectType = ({
+const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } }, // if you give me an ID I will return You user type
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id })
+      } // resolve function return the data
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
